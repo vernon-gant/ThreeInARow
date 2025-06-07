aa8ec1dbcb4ef3dc4b00b4d2143b84b3d3c20ebe
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-07 11:23:59 +0200

    chore(matching, t-matching tests): structure test file into positive and negative cases

--- tests/ThreeInARow.Grid.Matching.Tests/TMatchingStrategyTests.cs
+++ tests/ThreeInARow.Grid.Matching.Tests/TMatchingStrategyTests.cs
@@ -13,40 +13,18 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
         _strategy = new TMatchingStrategy<string>(3, _horizontalStrategy, _verticalStrategy);
     }
 
+    # region Negative Cases
+
     [Test]
     public void When_NoHorizontalOrVerticalMatches_Then_ReturnsNoTMatches()
     {
-        // Given - No matches in 8x8 grid
+        // Given - No matches in grid
         var grid = new[,]
         {
             { "A", "B", "A", "B", "A", "B", "A", "B" },
             { "B", "A", "B", "A", "B", "A", "B", "A" },
             { "A", "B", "A", "B", "A", "B", "A", "B" },
             { "B", "A", "B", "A", "B", "A", "B", "A" },
-            { "A", "B", "A", "B", "A", "B", "A", "B" },
-            { "B", "A", "B", "A", "B", "A", "B", "A" },
-            { "A", "B", "A", "B", "A", "B", "A", "B" },
-            { "B", "A", "B", "A", "B", "A", "B", "A" }
-        };
-        var cells = this.CreateCellsFromGrid(grid);
-
-        // When
-        var matches = _strategy.FindMatches(cells);
-
-        // Then
-        Assert.That(matches, Is.Empty);
-    }
-
-    [Test]
-    public void When_HorizontalAndVerticalMatchesDoNotIntersect_Then_ReturnsNoTMatches()
-    {
-        // Given - Separate horizontal and vertical matches that don't touch
-        var grid = new[,]
-        {
-            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match (row 0, cols 0-2)
-            { "G", "H", "I", "B", "J", "K", "L", "M" },
-            { "N", "O", "P", "B", "Q", "R", "S", "T" },
-            { "U", "V", "W", "B", "X", "Y", "Z", null }, // Vertical match (col 3, rows 0-3)
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -62,14 +40,38 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
     }
 
     [Test]
-    public void When_HorizontalAndVerticalMatchesIntersectAtEnd_Then_ReturnsNoTMatches()
+    public void When_MatchesDoNotIntersect_Then_ReturnsNoTMatches()
     {
-        // Given - L-shape pattern (intersection at corner, not T-shape)
+        // Given - Separate horizontal and vertical matches
         var grid = new[,]
         {
-            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match(row 0, cols 0-2)
-            { "A", "G", "H", "I", "J", "K", "L", "M" }, // Vertical match(col 0, rows 0-2)
-            { "A", "N", "O", "P", "Q", "R", "S", "T" }, // Intersection at (0,0) which is an END
+            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match
+            { "G", "H", "I", "B", "J", "K", "L", "M" },
+            { "N", "O", "P", "B", "Q", "R", "S", "T" },
+            { "U", "V", "W", "B", "X", "Y", "Z", null }, // Vertical match
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null }
+        };
+        var cells = this.CreateCellsFromGrid(grid);
+
+        // When
+        var matches = _strategy.FindMatches(cells);
+
+        // Then
+        Assert.That(matches, Is.Empty);
+    }
+
+    [Test]
+    public void When_LShapeExists_Then_ReturnsNoTMatches()
+    {
+        // Given - L-shape (intersection at corner, not middle)
+        var grid = new[,]
+        {
+            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match
+            { "A", "G", "H", "I", "J", "K", "L", "M" }, // Vertical match
+            { "A", "N", "O", "P", "Q", "R", "S", "T" }, // Corner intersection
             { "U", "V", "W", "X", "Y", "Z", null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -81,19 +83,71 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
         // When
         var matches = _strategy.FindMatches(cells);
 
-        // Then - Should be empty because intersection is at end, not middle (L-shape, not T-shape)
+        // Then
         Assert.That(matches, Is.Empty);
     }
 
     [Test]
-    public void When_TShapePointingDown_Then_ReturnsOneTMatch()
+    public void When_CrossShapeExists_Then_ReturnsNoTMatches()
     {
-        // Given - T-shape pointing down ⊤
+        // Given - Cross shape (intersection in middle of both lines)
         var grid = new[,]
         {
-            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal line (top of T)
-            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical stem going down
-            { "N", "A", "O", "P", "Q", "R", "S", "T" }, // Vertical stem continues
+            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical part above
+            { "A", "A", "A", "N", "O", "P", "Q", "R" }, // Horizontal through center
+            { "S", "A", "T", "U", "V", "W", "X", "Y" }, // Vertical part below
+            { "Z", null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null }
+        };
+        var cells = this.CreateCellsFromGrid(grid);
+
+        // When
+        var matches = _strategy.FindMatches(cells);
+
+        // Then
+        Assert.That(matches, Is.Empty);
+    }
+
+    [Test]
+    public void When_EmptyCellsBreakTShape_Then_ReturnsNoMatches()
+    {
+        // Given - T-shape broken by empty cell
+        var grid = new[,]
+        {
+            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match
+            { "G", null, "H", "I", "J", "K", "L", "M" }, // Empty cell breaks vertical
+            { "N", "A", "O", "P", "Q", "R", "S", "T" },
+            { "U", "V", "W", "X", "Y", "Z", null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null }
+        };
+        var cells = this.CreateCellsFromGrid(grid);
+
+        // When
+        var matches = _strategy.FindMatches(cells);
+
+        // Then
+        Assert.That(matches, Is.Empty);
+    }
+
+    # endregion
+
+    #region Positive Cases
+
+    [Test]
+    public void When_TShapePointingDown_Then_ReturnsOneTMatch()
+    {
+        // Given - T pointing down ⊤
+        var grid = new[,]
+        {
+            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal top
+            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical stem down
+            { "N", "A", "O", "P", "Q", "R", "S", "T" }, // Vertical continues
             { "U", "V", "W", "X", "Y", "Z", null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -113,12 +167,12 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
     [Test]
     public void When_TShapePointingUp_Then_ReturnsOneTMatch()
     {
-        // Given - T-shape pointing up ⊥
+        // Given - T pointing up ⊥
         var grid = new[,]
         {
-            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical stem going up
-            { "N", "A", "O", "P", "Q", "R", "S", "T" }, // Vertical stem continues
-            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal line (bottom of T)
+            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical stem up
+            { "N", "A", "O", "P", "Q", "R", "S", "T" }, // Vertical continues
+            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal bottom
             { "U", "V", "W", "X", "Y", "Z", null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -132,20 +186,19 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
 
         // Then
         Assert.That(matches, Has.Count.EqualTo(1));
-        Assert.That(matches[0], Is.TypeOf<TMatch<string>>());
         Assert.That(matches[0].Count, Is.EqualTo(5));
     }
 
     [Test]
     public void When_TShapePointingRight_Then_ReturnsOneTMatch()
     {
-        // Given - T-shape pointing right ⊣
+        // Given - T pointing right ⊣
         var grid = new[,]
         {
-            { "A", "G", "H", "I", "J", "K", "L", "M" },
-            { "A", "A", "A", "N", "O", "P", "Q", "R" }, // Horizontal stem going right
-            { "A", "S", "T", "U", "V", "W", "X", "Y" },
-            { "Z", ".", ".", ".", ".", ".", null, null },
+            { "A", "G", "H", "I", "J", "K", "L", "M" }, // Vertical left
+            { "A", "A", "A", "N", "O", "P", "Q", "R" }, // Horizontal stem right
+            { "A", "S", "T", "U", "V", "W", "X", "Y" }, // Vertical continues
+            { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -158,20 +211,19 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
 
         // Then
         Assert.That(matches, Has.Count.EqualTo(1));
-        Assert.That(matches[0], Is.TypeOf<TMatch<string>>());
         Assert.That(matches[0].Count, Is.EqualTo(5));
     }
 
     [Test]
     public void When_TShapePointingLeft_Then_ReturnsOneTMatch()
     {
-        // Given - T-shape pointing left ⊢
+        // Given - T pointing left ⊢
         var grid = new[,]
         {
-            { "G", "H", "I", "A", "J", "K", "L", "M" },
-            { "N", "A", "A", "A", "O", "P", "Q", "R" }, // Horizontal stem going left
-            { "S", "T", "U", "A", "V", "W", "X", "Y" },
-            { "Z", ".", ".", ".", ".", ".", null, null },
+            { "G", "H", "I", "A", "J", "K", "L", "M" }, // Vertical right
+            { "N", "A", "A", "A", "O", "P", "Q", "R" }, // Horizontal stem left
+            { "S", "T", "U", "A", "V", "W", "X", "Y" }, // Vertical continues
+            { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -184,20 +236,44 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
 
         // Then
         Assert.That(matches, Has.Count.EqualTo(1));
-        Assert.That(matches[0], Is.TypeOf<TMatch<string>>());
         Assert.That(matches[0].Count, Is.EqualTo(5));
     }
 
     [Test]
-    public void When_MultipleTShapesExist_Then_ReturnsAllTMatches()
+    public void When_LargerTShape_Then_ReturnsOneTMatch()
+    {
+        // Given - T with 5-cell horizontal, 4-cell vertical
+        var grid = new[,]
+        {
+            { "A", "A", "A", "A", "A", "B", "C", "D" }, // 5-cell horizontal
+            { "G", "H", "Y", "A", "J", "K", "L", "M" }, // Vertical down from middle
+            { "N", "O", "Z", "A", "Q", "R", "S", "T" }, // Vertical continues
+            { "U", "V", "B", "A", "Y", "Z", null, null }, // Vertical continues
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null }
+        };
+        var cells = this.CreateCellsFromGrid(grid);
+
+        // When
+        var matches = _strategy.FindMatches(cells);
+
+        // Then
+        Assert.That(matches, Has.Count.EqualTo(1));
+        Assert.That(matches[0].Count, Is.EqualTo(8)); // 5 + 4 - 1
+    }
+
+    [Test]
+    public void When_MultipleTShapes_Then_ReturnsAllTMatches()
     {
         // Given - Two separate T-shapes
         var grid = new[,]
         {
-            { "A", "A", "A", "B", "C", "C", "C", "D" }, // First T horizontal, Second T horizontal
-            { "E", "A", "F", "G", "H", "C", "I", "J" }, // First T stem down, Second T stem down
-            { "K", "L", "M", "N", "O", "P", "Q", "R" },
-            { "S", "T", "U", "V", "W", "X", "Y", "Z" },
+            { "A", "A", "A", "B", "C", "C", "C", "D" }, // Two horizontal matches
+            { "E", "A", "F", "G", "H", "C", "I", "J" }, // Two vertical stems
+            { "K", "A", "M", "N", "O", "C", "Q", "R" },
+            { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
             { null, null, null, null, null, null, null, null },
@@ -210,54 +286,7 @@ public class TMatchingStrategyTests : ICellsFromGridConverter
 
         // Then
         Assert.That(matches, Has.Count.EqualTo(2));
-        Assert.That(matches.All(m => m is TMatch<string>), Is.True);
     }
 
-    [Test]
-    public void When_GridHasEmptyCellsBreakingTMatch_Then_ReturnsNoMatches()
-    {
-        // Given - T-shape broken by empty cells
-        var grid = new[,]
-        {
-            { "A", "A", "A", "B", "C", "D", "E", "F" }, // Horizontal match
-            { "G", null, "H", "I", "J", "K", "L", "M" }, // Empty cell breaks vertical stem
-            { "N", "O", "P", "Q", "R", "S", "T", "U" },
-            { "V", "W", "X", "Y", "Z", ".", ".", "." },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null }
-        };
-        var cells = this.CreateCellsFromGrid(grid);
-
-        // When
-        var matches = _strategy.FindMatches(cells);
-
-        // Then
-        Assert.That(matches, Is.Empty);
-    }
-
-    [Test]
-    public void When_CrossShapeExists_Then_ReturnsNoTMatches()
-    {
-        // Given - Cross shape (NOT a T-shape) - this should NOT match
-        var grid = new[,]
-        {
-            { "G", "A", "H", "I", "J", "K", "L", "M" }, // Vertical part above center
-            { "A", "A", "A", "N", "O", "P", "Q", "R" }, // Horizontal line through center
-            { "S", "A", "T", "U", "V", "W", "X", "Y" }, // Vertical part below center
-            { "Z", ".", ".", ".", ".", ".", null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null }
-        };
-        var cells = this.CreateCellsFromGrid(grid);
-
-        // When
-        var matches = _strategy.FindMatches(cells);
-
-        // Then - Cross shape should NOT be detected as T-match
-        Assert.That(matches, Is.Empty);
-    }
+    #endregion
 }
