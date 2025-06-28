129e013d097be13c53c5bc71c5aeccf9328e0b9a
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-28 22:06:27 +0200

    new(statistics): base ADT

---
+++ src/ThreeInARow.Progression.Statistics/ADT/IStatistic.cs
@@ -0,0 +1,15 @@
+﻿using OneOf;
+using OneOf.Types;
+
+namespace ThreeInARow.Progression.Statistics.ADT;
+
+public interface IStatistic
+{
+    public string Name { get; }
+
+    public OneOf<string, None> Description { get; }
+
+    public string Value { get; }
+
+    public string Unit { get; }
+}
