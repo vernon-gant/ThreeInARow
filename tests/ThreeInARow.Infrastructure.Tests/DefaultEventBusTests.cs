cbf192868cc5948db85310d99f2bc0c55f434206
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-28 17:47:19 +0200

    test(infrastructure/EventBus): publishing event for registered handler

---
+++ tests/ThreeInARow.Infrastructure.Tests/DefaultEventBusTests.cs
@@ -0,0 +1,34 @@
+﻿using NSubstitute;
+using ThreeInARow.Infrastructure.ADT;
+using ThreeInARow.Infrastructure.Implementation;
+
+namespace ThreeInARow.Infrastructure.Tests;
+
+public class TestEvent : IEvent;
+
+[TestFixture]
+public class DefaultEventBusTests
+{
+    private DefaultEventBus _eventBus;
+
+    [SetUp]
+    public void Setup()
+    {
+        _eventBus = new DefaultEventBus();
+    }
+
+    [Test]
+    public void GivenBusWithOneRegisteredHandler_WhenPublishingEventForThisHandler_ThenHandlerIsInvoked()
+    {
+        // Given
+        var handler = Substitute.For<IEventHandler<TestEvent>>();
+        _eventBus.Register(handler);
+
+        // When
+        var @event = new TestEvent();
+        _eventBus.Publish(@event);
+
+        // Then
+        handler.Received(1).Handle(@event);
+    }
+}
