812eeede49db2e7345e975a258d45633858e1275
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-28 15:33:14 +0200

    test(utilities): added grid independent creation of a cell for testing purposes(and only for it)

---
+++ tests/ThreeInARow.TestingUtilities/CellUtilities.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute;
+using ThreeInARow.Grid.ADT;
+using ThreeInARow.Grid.ValueObjects;
+using OneOf;
+
+namespace ThreeInARow.TestingUtilities;
+
+public static class CellExtensions
+{
+    public static Cell<T> CreateTestCell<T>(T content, int row, int column)
+    {
+        var gridMock = Substitute.For<IReadableGrid<T>>();
+        var result = OneOf<CellContent<T>, CellOutOfBounds>.FromT0(content);
+        gridMock.ContentAt(row, column).Returns(result);
+        var cellResult = Cell<T>.FromGrid(gridMock, row, column);
+        return cellResult.AsT0;
+    }
+}
