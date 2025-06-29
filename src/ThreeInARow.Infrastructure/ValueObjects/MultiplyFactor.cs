e5c39fe6173fe3acb0c0713a8614b8cf57532f1e
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-29 11:37:02 +0200

    new(infrastructure): value objects for working with positive numbers

---
+++ src/ThreeInARow.Infrastructure/ValueObjects/MultiplyFactor.cs
@@ -0,0 +1,16 @@
+﻿namespace ThreeInARow.Infrastructure.ValueObjects;
+
+public readonly record struct MultiplyFactor
+{
+    public MultiplyFactor() => Value = 1;
+
+    public MultiplyFactor(int value)
+    {
+        if (Value < 1)
+            throw new ArgumentOutOfRangeException(nameof(value), "Value must be greater than or equal to 1.");
+
+        Value = value;
+    }
+
+    public int Value { get; }
+}
---
+++ src/ThreeInARow.Infrastructure/ValueObjects/PositiveInt.cs
@@ -0,0 +1,31 @@
+﻿using OneOf;
+using OneOf.Types;
+
+namespace ThreeInARow.Infrastructure.ValueObjects;
+
+public readonly record struct PositiveInt
+{
+    public PositiveInt() => Value = 0;
+
+    public PositiveInt(int value)
+    {
+        if (Value < 0)
+            throw new ArgumentOutOfRangeException(nameof(value), "Value must be greater than or equal to 0.");
+
+        Value = value;
+    }
+
+    public int Value { get; }
+
+    public PositiveInt Multiply(MultiplyFactor factor) => new (Value * factor.Value);
+
+    public OneOf<PositiveInt, Error> Subtract(PositiveInt other)
+    {
+        if (Value < other.Value)
+            return new Error();
+
+        return new PositiveInt(Value - other.Value);
+    }
+
+    public static PositiveInt operator +(PositiveInt left, PositiveInt right) => new (left.Value + right.Value);
+}
