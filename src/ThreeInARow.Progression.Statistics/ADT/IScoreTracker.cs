68aaac3e6c40f75f94de72857143f5338c91d27e
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-29 11:37:35 +0200

    new(progression/statistics): score tracker ADT

---
+++ src/ThreeInARow.Progression.Statistics/ADT/IScoreTracker.cs
@@ -0,0 +1,12 @@
+﻿using ThreeInARow.Infrastructure.ValueObjects;
+
+namespace ThreeInARow.Progression.Statistics.ADT;
+
+public interface IScoreTracker
+{
+    PositiveInt CurrentScore { get; }
+
+    void IncrementScore(PositiveInt increment);
+
+    void ResetScore();
+}
