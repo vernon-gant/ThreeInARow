dd4183014a53f86a83141ba2f07af170450f305f
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-30 08:52:57 +0200

    feat(statistics): TotalMatches statistic to count all matches including cascades

---
+++ src/ThreeInARow.Progression.Statistics/Implementation/Statistics/General/TotalMatches.cs
@@ -0,0 +1,25 @@
+﻿using OneOf;
+using OneOf.Types;
+using ThreeInARow.Infrastructure.ADT;
+using ThreeInARow.Progression.Events.Events;
+using ThreeInARow.Progression.Statistics.ADT;
+
+namespace ThreeInARow.Progression.Statistics.Implementation.Statistics.General;
+
+public class TotalMatches<TElement> : IStatistic, IEventHandler<MatchFound<TElement>> where TElement : IEquatable<TElement>
+{
+    private int _totalMatches;
+
+    public string Name => "Total Matches";
+
+    public OneOf<string, None> Description => "Counts all matches including cascades";
+
+    public OneOf<string, None> Unit => new None();
+
+    public OneOf<string, NotEnoughData> Value => _totalMatches.ToString();
+
+    public void Handle(MatchFound<TElement> notification)
+    {
+        _totalMatches++;
+    }
+}
---
+++ tests/ThreeInARow.Progression.Statistics.Tests/TotalMatchesTests.cs
@@ -0,0 +1,271 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using OneOf.Types;
+using ThreeInARow.Grid.Matching.ADT;
+using ThreeInARow.Progression.Events.Events;
+using ThreeInARow.Progression.Statistics.Implementation.Statistics.General;
+using ThreeInARow.TestingUtilities;
+
+namespace ThreeInARow.Progression.Statistics.Tests;
+
+[TestFixture]
+public class TotalMatchesTests
+{
+    private TotalMatches<string> _totalMatches;
+    private IMatch<string> _mockMatch;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _totalMatches = new TotalMatches<string>();
+        _mockMatch = Substitute.For<IMatch<string>>();
+    }
+
+    #region Basic Property Tests
+
+    [Test]
+    public void GivenTotalMatches_WhenGettingName_ThenReturnsCorrectName()
+    {
+        // Given
+        // When
+        var name = _totalMatches.Name;
+
+        // Then
+        name.Should().Be("Total Matches");
+    }
+
+    [Test]
+    public void GivenTotalMatches_WhenGettingDescription_ThenReturnsCorrectDescription()
+    {
+        // Given
+        // When
+        var description = _totalMatches.Description;
+
+        // Then
+        description.ShouldBeOfTypeOneOf<string>();
+        description.AsT0.Should().Be("Counts all matches including cascades");
+    }
+
+    [Test]
+    public void GivenTotalMatches_WhenGettingUnit_ThenReturnsNone()
+    {
+        // Given
+        // When
+        var unit = _totalMatches.Unit;
+
+        // Then
+        unit.ShouldBeOfTypeOneOf<None>();
+    }
+
+    #endregion
+
+    #region Value Property Tests
+
+    [Test]
+    public void GivenNoMatchesHandled_WhenGettingValue_ThenReturnsZero()
+    {
+        // Given
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.ShouldBeOfTypeOneOf<string>();
+        value.AsT0.Should().Be("0");
+    }
+
+    [Test]
+    public void GivenOneDirectMatchFound_WhenGettingValue_ThenReturnsOne()
+    {
+        // Given
+        var matchEvent = new MatchFound<string>
+        {
+            Match = _mockMatch,
+            IsCascade = false
+        };
+        _totalMatches.Handle(matchEvent);
+
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.IsT0.Should().BeTrue();
+        value.AsT0.Should().Be("1");
+    }
+
+    [Test]
+    public void GivenOneCascadeMatchFound_WhenGettingValue_ThenReturnsOne()
+    {
+        // Given
+        var matchEvent = new MatchFound<string>
+        {
+            Match = _mockMatch,
+            IsCascade = true
+        };
+        _totalMatches.Handle(matchEvent);
+
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.IsT0.Should().BeTrue();
+        value.AsT0.Should().Be("1");
+    }
+
+    [Test]
+    public void GivenMultipleDirectMatches_WhenGettingValue_ThenReturnsCorrectCount()
+    {
+        // Given
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.IsT0.Should().BeTrue();
+        value.AsT0.Should().Be("3");
+    }
+
+    [Test]
+    public void GivenMultipleCascadeMatches_WhenGettingValue_ThenReturnsCorrectCount()
+    {
+        // Given
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.IsT0.Should().BeTrue();
+        value.AsT0.Should().Be("3");
+    }
+
+    [Test]
+    public void GivenMixedDirectAndCascadeMatches_WhenGettingValue_ThenReturnsCorrectTotalCount()
+    {
+        // Given
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+        _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+
+        // When
+        var value = _totalMatches.Value;
+
+        // Then
+        value.IsT0.Should().BeTrue();
+        value.AsT0.Should().Be("5", "should count all matches regardless of cascade status");
+    }
+
+    #endregion
+
+    #region Event Handling Tests
+
+    [Test]
+    public void GivenMatchFoundEventDirect_WhenHandling_ThenIncrementsCounter()
+    {
+        // Given
+        var initialValue = int.Parse(_totalMatches.Value.AsT0);
+        var matchEvent = new MatchFound<string>
+        {
+            Match = _mockMatch,
+            IsCascade = false
+        };
+
+        // When
+        _totalMatches.Handle(matchEvent);
+
+        // Then
+        var newValue = int.Parse(_totalMatches.Value.AsT0);
+        newValue.Should().Be(initialValue + 1);
+    }
+
+    [Test]
+    public void GivenMatchFoundEventCascade_WhenHandling_ThenIncrementsCounter()
+    {
+        // Given
+        var initialValue = int.Parse(_totalMatches.Value.AsT0);
+        var matchEvent = new MatchFound<string>
+        {
+            Match = _mockMatch,
+            IsCascade = true
+        };
+
+        // When
+        _totalMatches.Handle(matchEvent);
+
+        // Then
+        var newValue = int.Parse(_totalMatches.Value.AsT0);
+        newValue.Should().Be(initialValue + 1);
+    }
+
+    [Test]
+    public void GivenSequenceOfMatches_WhenHandling_ThenCountsAllMatchesRegardlessOfCascadeStatus()
+    {
+        // Given
+        var matches = new[]
+        {
+            new MatchFound<string> { Match = _mockMatch, IsCascade = false },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = true },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = true },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = false },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = true },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = false },
+            new MatchFound<string> { Match = _mockMatch, IsCascade = false }
+        };
+
+        // When
+        foreach (var match in matches)
+        {
+            _totalMatches.Handle(match);
+        }
+
+        // Then
+        _totalMatches.Value.AsT0.Should().Be("7", "should count all matches in the sequence");
+    }
+
+    [Test]
+    public void GivenLargeNumberOfMatches_WhenHandling_ThenMaintainsAccurateCount()
+    {
+        // Given
+        const int totalMatchCount = 1000;
+        const int cascadeMatches = 400;
+
+        // When
+        for (int i = 0; i < cascadeMatches; i++)
+        {
+            _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = true });
+        }
+
+        for (int i = 0; i < (totalMatchCount - cascadeMatches); i++)
+        {
+            _totalMatches.Handle(new MatchFound<string> { Match = _mockMatch, IsCascade = false });
+        }
+
+        // Then
+        _totalMatches.Value.AsT0.Should().Be(totalMatchCount.ToString());
+    }
+
+    [Test]
+    public void GivenDifferentMatchTypes_WhenHandling_ThenCountsAllMatches()
+    {
+        // Given
+        var horizontalMatch = Substitute.For<IMatch<string>>();
+        var verticalMatch = Substitute.For<IMatch<string>>();
+        var tShapeMatch = Substitute.For<IMatch<string>>();
+
+        // When
+        _totalMatches.Handle(new MatchFound<string> { Match = horizontalMatch, IsCascade = false });
+        _totalMatches.Handle(new MatchFound<string> { Match = verticalMatch, IsCascade = true });
+        _totalMatches.Handle(new MatchFound<string> { Match = tShapeMatch, IsCascade = false });
+
+        // Then
+        _totalMatches.Value.AsT0.Should().Be("3", "should count all match types");
+    }
+
+    #endregion
+}
