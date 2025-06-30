4a37a74afa449f6febf308ffb6d54f74c2b6066d
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-30 11:45:52 +0200

    new(game timer): thin wrapper around stopwatch to make it more domain specific

---
+++ src/ThreeInARow.Infrastructure/ADT/IGameTimer.cs
@@ -0,0 +1,21 @@
+﻿using OneOf;
+using OneOf.Types;
+
+namespace ThreeInARow.Infrastructure.ADT;
+
+public interface IGameTimer
+{
+    OneOf<Success, GameRunning> StartGame();
+
+    OneOf<Success, GameNotRunning> StopGame();
+
+    bool IsGameRunning { get; }
+
+    bool HasGameEnded  { get; }
+
+    OneOf<TimeSpan, GameNotRunning> ElapsedGameTime { get; }
+}
+
+public struct GameRunning;
+
+public struct GameNotRunning;
